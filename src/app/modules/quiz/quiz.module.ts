529fdc1ee00edbc89e781f2a6a9d19f118c3c0fb
vvacarov <vvacarov@pentalog.fr>
2017-08-17 18:08:58 +0300

    add jQuery, need to use them, make first modal. it look worst

--- src/app/modules/quiz/quiz.module.ts
+++ src/app/modules/quiz/quiz.module.ts
@@ -2,11 +2,20 @@ import { QuizService } from './quiz.service';
 import { NgModule, Input, Output } from '@angular/core';
 import { routing } from 'app/modules/quiz/quiz.routes';
 
-import {MdRadioModule,MdCardModule, MdButtonModule, MdPaginatorModule} from '@angular/material';
-import { UserResultsPageComponent } from './pages/user-results/user-results.page'
-
+import { MdRadioModule,MdCardModule, MdButtonModule, MdPaginatorModule } from '@angular/material';
 import { SharedModule } from 'app/shared/shared.module';
 import { Ng2OrderModule } from 'ng2-order-pipe';
+
+import { UserResultsPageComponent } from './pages/user-results/user-results.page';
+import { NgbdModalContent } from './pages/user-results/modal-component';
+
+// import * as jQuery from 'jquery/dist/jquery';
+
+
+// import { NgbdModalContent, NgbdModalComponent } from './components/modal-result.component'
+
+
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 
 @NgModule({
@@ -17,14 +26,18 @@ import { Ng2OrderModule } from 'ng2-order-pipe';
         MdCardModule,
         MdButtonModule,
         MdPaginatorModule,
-        Ng2OrderModule
+        Ng2OrderModule,
+        NgbModule.forRoot()
+        // jQuery
     ],
     declarations: [
         UserResultsPageComponent,
+        NgbdModalContent
     ],
     providers: [
         QuizService
-    ]
+    ],
+      entryComponents: [NgbdModalContent]
 })
 export class QuizModule {
 
--- src/app/modules/quiz/quiz.routes.ts
+++ src/app/modules/quiz/quiz.routes.ts
@@ -11,7 +11,7 @@ const routes: Routes = [
     // },
     {
         path: 'user-results',
-        component: UserResultsPageComponent,
+        component: UserResultsPageComponent
         // canActivate: [AuthenticatedGuard],
     }
 ];
